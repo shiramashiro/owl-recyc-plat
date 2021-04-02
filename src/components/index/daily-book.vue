5c58116dbeb97336da41cb80015bf8984d012c40
kongsa <www.zhengrenfu@hotmail.com>
2021-04-03 01:54:42 +0800

    :heavy_plus_sign: 完成新书上架模块

---
+++ src/components/index/daily-book.vue
@@ -0,0 +1,67 @@
+<template>
+    <view
+        @click="viewDetails()"
+        class="daily-book margin-right-sm margin-left-sm padding-sm"
+    >
+        <view class="row-1">
+            <image mode="aspectFill" class="cover" :src="data.cover"></image>
+        </view>
+        <view class="row-2 text-xs">
+            {{ data.name }}
+        </view>
+        <view class="row-3 margin-top-sm text-red text-bold">
+            {{ data.originPrice }}
+        </view>
+    </view>
+</template>
+
+<script>
+export default {
+    name: 'DailyBook',
+    data() {
+        return {}
+    },
+    props: {
+        data: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        viewDetails() {
+            uni.navigateTo({
+                url: '/pages/index/sub-index?id=' + this.data.id
+            })
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.daily-book {
+    background-color: white;
+    border-radius: 15rpx;
+
+    .row-1 {
+        .cover {
+            width: 220rpx;
+            height: 300rpx;
+        }
+    }
+
+    .row-3::before {
+        content: '¥';
+        font-size: 80%;
+        margin-right: 4rpx;
+    }
+
+    .row-2 {
+        display: -webkit-box;
+        word-break: break-all;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+}
+</style>
--- src/components/index/home-tab.vue
+++ src/components/index/home-tab.vue
@@ -11,8 +11,10 @@
                 </swiper-item>
             </swiper>
         </view>
-        <view class="row-2 margin-bottom-xs">
-            <view class="row-2-1 flex justify-between align-center">
+        <view class="row-2">
+            <view
+                class="text-white row-2-1 margin-left-xs margin-right-xs padding-top-lg padding-bottom-lg padding-left-sm padding-right-sm flex justify-between align-center"
+            >
                 <view class="col-1 flex text-lg">
                     <view class="col-1-1 text-lg">
                         最受欢迎书籍日榜
@@ -22,28 +24,73 @@
                     </view>
                 </view>
                 <view class="col-2">
-                    <view class="col-3">
-                        <button size="mini" disabled="true">更多</button>
+                    <view class="col-2-1 text-xs">
+                        更多
                     </view>
                 </view>
             </view>
-            <view class="row-2">
-                <!-- 组件，宫格组件 -->
-            </view>
+            <scroll-view scroll-x="true" class="row-2-2 margin-bottom-lg">
+                <view class="rows flex">
+                    <daily-book
+                        v-for="(book, index) in dailyBooks"
+                        :key="index"
+                        :data="book"
+                    ></daily-book>
+                </view>
+            </scroll-view>
         </view>
     </view>
 </template>
 
 <script>
+import DailyBook from './daily-book.vue'
+
 export default {
     name: 'HomeTab',
-    components: {},
+    components: { DailyBook },
     data() {
         return {
             slideShow: [
                 'https://interweave.oss-cn-chengdu.aliyuncs.com/static/img/2021032611362390127.jpg',
                 'https://interweave.oss-cn-chengdu.aliyuncs.com/static/img/20210326193508509.jpg',
                 'https://interweave.oss-cn-chengdu.aliyuncs.com/static/img/2021032619353510220.jpg'
+            ],
+            dailyBooks: [
+                {
+                    id: 7,
+                    cover:
+                        'https://interweave.oss-cn-chengdu.aliyuncs.com/static/img/21114192-1_u_3.jpg',
+                    name: '新华字典（第11版）（单色本）',
+                    originPrice: '19.90'
+                },
+                {
+                    id: 8,
+                    cover:
+                        'https://interweave.oss-cn-chengdu.aliyuncs.com/static/img/23890604-1_w_5.jpg',
+                    name: '中华成语词典(第3版,单色插图本)',
+                    originPrice: '45.00'
+                },
+                {
+                    id: 9,
+                    cover:
+                        'https://interweave.oss-cn-chengdu.aliyuncs.com/static/img/25294671-1_w_3.jpg',
+                    name: '学生实用古汉语常用字字典（第6版）',
+                    originPrice: '32.00'
+                },
+                {
+                    id: 5,
+                    cover:
+                        'https://interweave.oss-cn-chengdu.aliyuncs.com/static/img/29142965-1_w_3.jpg',
+                    name: 'Python零基础项目开发快速入门',
+                    originPrice: '79.80'
+                },
+                {
+                    id: 6,
+                    cover:
+                        'https://interweave.oss-cn-chengdu.aliyuncs.com/static/img/29198330-1_w_3.jpg',
+                    name: 'Spring 5 设计模式',
+                    originPrice: '79.00'
+                }
             ]
         }
     }
@@ -56,6 +103,33 @@ export default {
         .slide-show {
             width: 100%;
             height: 100%;
+        }
+    }
+
+    .row-2 {
+        .row-2-1 {
+            background-color: rgb(106, 173, 252);
+            border-radius: 24rpx 24rpx 2rpx 2rpx;
+
+            .col-2 {
+                .col-2-1 {
+                    border-radius: 50rpx;
+                    border: 1px solid rgb(103, 160, 249);
+                    padding: 4rpx 40rpx;
+                    background-color: rgb(103, 160, 249);
+                }
+
+                .col-2-1::after {
+                    font-family: 'iconfont' !important;
+                    font-style: normal;
+                    -webkit-font-smoothing: antialiased;
+                    -moz-osx-font-smoothing: grayscale;
+                    content: '\e78a';
+                }
+            }
+        }
+
+        .row-2-1 {
         }
     }
 }
