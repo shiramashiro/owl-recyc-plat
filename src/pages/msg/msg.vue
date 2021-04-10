92306d991bf3a7e82fb5c0abf2792a3e65636f72
kongsa <www.zhengrenfu@hotmail.com>
2021-04-10 17:09:34 +0800

    :white_check_mark: 完成路由跳转

--- src/pages/msg/msg.vue
+++ src/pages/msg/msg.vue
@@ -1,200 +1,234 @@
 <template>
-  <view>
-    <view class="top">
-     <view class="topChild">
-       <img src="../../static/回复.svg" alt="">
-       <text>回复我的</text>
-     </view>
-
-      <view class="topChild">
-        <img src="../../static/赞.svg" alt="">
-        <text>收到的赞</text>
-      </view>
-
-      <view class="topChild">
-        <img src="../../static/通知.svg" alt="">
-        <text>系统通知</text>
-      </view>
-
-    </view>
-
-    <view class="chart">
-
-      <view class="chartTop">聊天列表</view>
-
-      <view class="item" v-for="item in list">
-        <view class="leftImg">
-          <img :src="item.url" alt="">
+    <view class="msg">
+        <view class="top padding-lr-lg flex justify-between align-center">
+            <navigator :url="'/pages/msg/reply'">
+                <view class="flex align-center justify-center">
+                    <image
+                        style="width: 70rpx; height: 70rpx"
+                        mode="aspectFill"
+                        src="@/static/回复.svg"
+                    />
+                </view>
+                <view class="text-gray">
+                    回复我的
+                </view>
+            </navigator>
+            <navigator :url="'/pages/msg/praise'">
+                <view class="flex align-center justify-center">
+                    <image
+                        style="width: 70rpx; height: 70rpx"
+                        mode="aspectFill"
+                        src="@/static/赞.svg"
+                    />
+                </view>
+                <view class="text-gray">
+                    收到的赞
+                </view>
+            </navigator>
+            <navigator :url="'/pages/msg/sys-msg'">
+                <view class="flex align-center justify-center">
+                    <image
+                        style="width: 70rpx; height: 70rpx"
+                        mode="aspectFill"
+                        src="@/static/通知.svg"
+                    />
+                </view>
+                <view class="text-gray">
+                    系统通知
+                </view>
+            </navigator>
         </view>
-
-        <view class="rightMessage">
-
-          <view class="title">
-            <view class="name">{{item.name}}</view>
-            <view class="time">{{item.time}}</view>
-          </view>
-
-          <view class="detail">
-            {{item.detail}}
-          </view>
-
-        </view>
-      </view>
-
-    </view>
-
-    <view class="content">
-      <view class="cu-list menu-avatar" v-for="item in list">
-        <view class="cu-item ">
-          <view class="cu-avatar radius lg" >
-            <img :src="item.url" alt="">
-          </view>
-          <view class="content">
-            <view class="text-pink">
-              <text class="text-cut">{{item.name}}</text>
+        <view class="chart">
+            <view
+                class="cu-list menu-avatar"
+                v-for="(item, index) in list"
+                :key="index"
+            >
+                <view class="cu-item">
+                    <view class="cu-avatar radius lg">
+                        <img :src="item.url" alt="" />
+                    </view>
+                    <view class="content">
+                        <view class="text-pink">
+                            <text class="text-cut">{{ item.name }}</text>
+                        </view>
+                        <view class="text-gray text-sm flex">
+                            <text class="text-cut">{{ item.detail }}</text>
+                        </view>
+                    </view>
+                    <view class="action">
+                        <view class="text-grey text-xs">
+                            {{ item.time }}
+                        </view>
+                        <template v-if="item.new > 0">
+                            <view class="cu-tag round bg-red sm">
+                                {{ item.new }}
+                            </view>
+                        </template>
+                    </view>
+                </view>
             </view>
-            <view class="text-gray text-sm flex">
-              <text class="text-cut">{{item.detail}}</text>
-            </view>
-          </view>
-          <view class="action">
-            <view class="text-grey text-xs">{{item.time}}</view>
-            <view class="cu-tag round bg-red sm">1</view>
-          </view>
         </view>
-
-      </view>
     </view>
-
-  </view>
 </template>
 
 <script>
 export default {
-name: "msg",
-  data(){
-    return{
-      list:[
-        {
-          url:"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2621980968,324835243&fm=26&gp=0.jpg",
-          name:"哔哩哔哩创作中心",
-          time:"04-05",
-          detail:"数据存储进行了性能优化，它的具体优化方式和升级注意事项"
-        },
-        {
-          url:"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2621980968,324835243&fm=26&gp=0.jpg",
-          name:"哔哩哔哩创作中心",
-          time:"04-05",
-          detail:"数据存储进行了性能优化，它的具体优化方式和升级注意事项"
-        },
-
-
-
-      ],
-
+    name: 'msg',
+    data() {
+        return {
+            list: [
+                {
+                    url:
+                        'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2621980968,324835243&fm=26&gp=0.jpg',
+                    name: '哔哩哔哩创作中心',
+                    time: '04-05',
+                    detail:
+                        '数据存储进行了性能优化，它的具体优化方式和升级注意事项',
+                    new: 0
+                },
+                {
+                    url:
+                        'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2621980968,324835243&fm=26&gp=0.jpg',
+                    name: '哔哩哔哩创作中心',
+                    time: '04-05',
+                    detail:
+                        '数据存储进行了性能优化，它的具体优化方式和升级注意事项',
+                    new: 0
+                },
+                {
+                    url:
+                        'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2621980968,324835243&fm=26&gp=0.jpg',
+                    name: '哔哩哔哩创作中心',
+                    time: '04-05',
+                    detail:
+                        '数据存储进行了性能优化，它的具体优化方式和升级注意事项',
+                    new: 0
+                },
+                {
+                    url:
+                        'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2621980968,324835243&fm=26&gp=0.jpg',
+                    name: '哔哩哔哩创作中心',
+                    time: '04-05',
+                    detail:
+                        '数据存储进行了性能优化，它的具体优化方式和升级注意事项',
+                    new: 1
+                },
+                {
+                    url:
+                        'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2621980968,324835243&fm=26&gp=0.jpg',
+                    name: '哔哩哔哩创作中心',
+                    time: '04-05',
+                    detail:
+                        '数据存储进行了性能优化，它的具体优化方式和升级注意事项',
+                    new: 20
+                },
+                {
+                    url:
+                        'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2621980968,324835243&fm=26&gp=0.jpg',
+                    name: '哔哩哔哩创作中心',
+                    time: '04-05',
+                    detail:
+                        '数据存储进行了性能优化，它的具体优化方式和升级注意事项',
+                    new: 10
+                },
+                {
+                    url:
+                        'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2621980968,324835243&fm=26&gp=0.jpg',
+                    name: '哔哩哔哩创作中心',
+                    time: '04-05',
+                    detail:
+                        '数据存储进行了性能优化，它的具体优化方式和升级注意事项',
+                    new: 10
+                },
+                {
+                    url:
+                        'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2621980968,324835243&fm=26&gp=0.jpg',
+                    name: '哔哩哔哩创作中心',
+                    time: '04-05',
+                    detail:
+                        '数据存储进行了性能优化，它的具体优化方式和升级注意事项',
+                    new: 10
+                }
+            ]
+        }
     }
-  }
 }
 </script>
 
-<style scoped>
-.cu-avatar ,.radius ,.lg{
-  border-radius: 50%;
-
+<style lang="css" scoped>
+.cu-avatar,
+.radius,
+.lg {
+    border-radius: 50%;
 }
-.cu-avatar ,.radius ,.lg img{
-  width: 43px;
-  height: 49px;
-  border-radius: 50%;
+.cu-avatar,
+.radius,
+.lg img {
+    width: 100rpx;
+    height: 100rpx;
+    border-radius: 100%;
 }
-.top{
-  width: 100%;
-  height: 60px;
-  background-color: #ffffff;
-  box-shadow: 2px 2px 5px #e5e0e0;
-  display: flex;
+.top {
+    position: sticky;
+    top: 0;
+    z-index: 998;
+    width: 100%;
+    height: 120rpx;
+    background-color: #ffffff;
+    box-shadow: 8rpx 8rpx 10rpx #e5e0e0;
 }
-.topChild{
-  width: 33%;
-  /*flex: 1;*/
-display: flex;
-/*  align-items: center;*/
-  justify-content: center;
-
+.item {
+    height: 70px;
+    width: 100%;
+    display: flex;
 }
-.topChild img{
-  display:inline-block;
-height: 60%;
+.leftImg {
+    width: 15%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
-.topChild text{
-  position: absolute;
-  top: 35px;
+.leftImg img {
+    width: 80%;
+    height: 70%;
+    border-radius: 50%;
 }
-.chart{
-  margin-top: 10px;
+.rightMessage {
+    width: 85%;
+    height: 100%;
+    border-bottom: 1px #e7e4e4 solid;
 }
-.chartTop{
-  background-color: #ffffff;
-  height: 30px;
-  padding-top: 6px;
-  padding-left: 16px;
-  border-bottom: 1px #cccccc solid;
-  color: #333333;
-
+.title {
+    width: 100%;
+    height: 23px;
+    margin-top: 10px;
+    display: flex;
 }
-.item{
-  height: 70px;
-  width: 100%;
-
-  display: flex;
+.name {
+    width: 86%;
+    color: deeppink;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding-top: 2px;
+    padding-left: 4px;
 }
-.leftImg{
-  width: 15%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
+.time {
+    width: 14%;
+    background-color: white;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding-top: 5px;
+    padding-left: 3px;
 }
-.leftImg img{
-  width: 80%;
-  height: 70%;
-  border-radius: 50%;
-}
-.rightMessage{
-  width: 85%;
-  height: 100%;
-  border-bottom: 1px #e7e4e4 solid;
-}
-.title{
-  width: 100%;
-  height: 23px;
-  margin-top: 10px;
-  display: flex;
-}
-.name{
-  width: 86%;
-  color: deeppink;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  padding-top: 2px;
-  padding-left: 4px;
-}
-.time{
-  width: 14%;
-  background-color: white;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  padding-top: 5px;
-  padding-left: 3px;
-}
-.detail{
-  margin-left: 4px;
-  margin-top: 8px;
-  width: 85%;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  font-size: 10px;
+.detail {
+    margin-left: 4px;
+    margin-top: 8px;
+    width: 85%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    font-size: 10px;
 }
 </style>
--- src/pages/msg/sys-msg.vue
+++ src/pages/msg/sys-msg.vue
@@ -1,97 +1,93 @@
 <template>
- <view>
-   <view class="top">
-     全部消息
-     <text>编辑</text>
-   </view>
+    <view>
+        <view class="top">
+            全部消息
+            <text>编辑</text>
+        </view>
 
+        <view class="item" v-for="item in list">
+            <view class="title">{{ item.title }}</view>
 
-   <view class="item" v-for="item in list">
-     <view class="title">{{item.title}}</view>
+            <view class="detail">
+                {{ item.detail }}
+                <text>{{ item.text }}</text>
+            </view>
 
-     <view class="detail">
-       {{item.detail}}
-       <text>{{item.text}}</text>
-     </view>
-
-     <view class="time">{{item.time}}</view>
-   </view>
-
- </view>
+            <view class="time">{{ item.time }}</view>
+        </view>
+    </view>
 </template>
 
 <script>
 export default {
-  name: "sys-msg",
-  data(){
-    return{
-    list:[
-      {
-        title:"终于揭晓了,B站跨年晚会即将开始",
-        detail:`目前英国各大标志建筑物已经降半旗致哀，在王室大型典礼中扮演重要角色的英国纹章院表示，
+    name: 'sys-msg',
+    data() {
+        return {
+            list: [
+                {
+                    title: '终于揭晓了,B站跨年晚会即将开始',
+                    detail: `目前英国各大标志建筑物已经降半旗致哀，在王室大型典礼中扮演重要角色的英国纹章院表示，
        菲利普亲王的葬礼将在温莎城堡内 圣乔治教堂举行，不会举行国葬和遗体瞻仰仪式
        ，呼吁公众不要参加菲利普亲王葬礼的相关活动`,
-        text:"&&网页链接",
-        time:"3个月前"
-      },
-      {
-        title:"直播即将开始",
-        detail:`目前英国各大标志建筑物已经降半旗致哀，在王室大型典礼中扮演重要角色的英国纹章院表示，
+                    text: '&&网页链接',
+                    time: '3个月前'
+                },
+                {
+                    title: '直播即将开始',
+                    detail: `目前英国各大标志建筑物已经降半旗致哀，在王室大型典礼中扮演重要角色的英国纹章院表示，
       ，不会举行国葬和遗体瞻仰仪式
        ，呼吁公众不要参加菲利普亲王葬礼的相关活动`,
-        text:"&&网页链接",
-        time:"3个月前"
-      },
-      {
-        title:"你的B站2020年年度报告",
-        detail:`目前英国各大标志建经降半旗致哀，在王室大型典礼中扮演重要角色的英国纹章院表示，
+                    text: '&&网页链接',
+                    time: '3个月前'
+                },
+                {
+                    title: '你的B站2020年年度报告',
+                    detail: `目前英国各大标志建经降半旗致哀，在王室大型典礼中扮演重要角色的英国纹章院表示，
        菲利普亲王的葬礼将在温 圣乔治教堂举行，不会举行国葬和遗体瞻仰仪式
        ，呼吁公众不要参加菲利普亲王葬礼的相关活动`,
-        text:"&&网页链接",
-        time:"3个月前"
-      }
-    ]
+                    text: '&&网页链接',
+                    time: '3个月前'
+                }
+            ]
+        }
     }
-  }
-
 }
 </script>
 
 <style scoped>
-.top{
-  height: 40px;
-  width: 100%;
-  padding-top: 12px;
-  padding-left: 16px;
-  position: relative;
-  border-bottom: 1px #ece9e9 solid;
+.top {
+    height: 40px;
+    width: 100%;
+    padding-top: 12px;
+    padding-left: 16px;
+    position: relative;
+    border-bottom: 1px #ece9e9 solid;
 }
-.top text{
-  position: absolute;
-  right: 15px;
-  color: deeppink;
+.top text {
+    position: absolute;
+    right: 15px;
+    color: deeppink;
 }
-.item{
-
-  width: 100%;
-  border-bottom: 1px #eae8e8 solid;
+.item {
+    width: 100%;
+    border-bottom: 1px #eae8e8 solid;
 }
-.title{
-  font-weight: bold;
-  padding-top: 12px;
-  padding-left: 16px;
+.title {
+    font-weight: bold;
+    padding-top: 12px;
+    padding-left: 16px;
 }
-.detail{
-  margin-top: 10px;
+.detail {
+    margin-top: 10px;
 }
-.detail text{
-  color: #4a67d6;
+.detail text {
+    color: #4a67d6;
 }
-.time{
-  margin-top: 10px;
-  padding-bottom: 10px;
-  margin-right: 10px;
-  opacity: 0.4;
-  margin-left: 80%;
+.time {
+    margin-top: 10px;
+    padding-bottom: 10px;
+    margin-right: 10px;
+    opacity: 0.4;
+    margin-left: 80%;
 }
 </style>
