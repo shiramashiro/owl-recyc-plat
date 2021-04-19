5cccc193501fbf37516a92d739c8ea3776d9aaea
woshiyuanhang <2587216169@qq.com>
2021-04-19 19:46:23 +0800

    注册页面

---
+++ src/pages/Sign/duanxin.vue
@@ -0,0 +1,199 @@
+<template>
+<view class="container">
+ <view class="title" @click="change" v-if="isShow">{{message1}}</view>
+  <view class="title" @click="change" v-else>{{message2}}</view>
+
+  <view class="box1" v-if="isShow">
+    <view class="image">
+       <view>
+         <image  mode="scaleToFill  " src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F7cf333adcb1562190d5be0b57803e37dfb26627615ad9c-kXca6I_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621423255&t=c3943783d9744feb3c4837a4e2e475a6" alt="">
+         </image>
+       </view>
+    </view>
+
+    <view class="list">
+
+       <view class="item">
+      <view class="title">
+        <view>账号</view>
+      </view>
+
+      <view class="input"><input type="number" placeholder="请输入手机号"></view>
+      <view class="tips"></view>
+    </view>
+
+       <view class="item">
+        <view class="title">
+          <view>密码</view>
+        </view>
+
+        <view class="input"><input type="number" placeholder="请输入密码"></view>
+        <view class="tips">忘记密码?</view>
+      </view>
+    </view>
+
+    <view class="box1Btn">
+      <button class="btn1">注册</button>
+      <button class="btn2">登录</button>
+    </view>
+
+  </view>
+
+  <view class="box2" v-else>
+    <view class="image">
+      <view>
+        <image  mode="scaleToFill " src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F7cf333adcb1562190d5be0b57803e37dfb26627615ad9c-kXca6I_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621423255&t=c3943783d9744feb3c4837a4e2e475a6" alt="">
+        </image>
+      </view>
+    </view>
+
+    <view class="list">
+
+      <view class="item">
+        <view class="title">
+          <view>中国大陆</view>
+        </view>
+
+        <view class="input"></view>
+        <view class="tips" style="color: black;padding-left: 20rpx;font-size: 32rpx">> </view>
+      </view>
+
+      <view class="item">
+        <view class="title">
+          <view>+86</view>
+        </view>
+
+        <view class="input"><input type="number" placeholder="请输入手机号"></view>
+        <view class="tips" style="color: #cccccc;border-left: 1px solid #cccccc">获取验证码</view>
+      </view>
+
+      <view class="item">
+        <view class="title">
+          <view>验证码</view>
+        </view>
+
+        <view class="input"><input type="number" placeholder="请输入验证码"></view>
+        <view class="tips" style="color: #cccccc"></view>
+      </view>
+    </view>
+
+    <view class="box2Btn">
+      <button>验证登录</button>
+    </view>
+
+
+
+  </view>
+
+
+
+</view>
+</template>
+
+<script>
+export default {
+  name: "duanxin",
+  data(){
+    return{
+      message1:"短信登录",
+      message2:"密码登录",
+      isShow:true
+    }
+  },
+  methods:{
+    change(){
+     this.isShow=!this.isShow
+    }
+  }
+}
+</script>
+
+<style scoped>
+.box1,.box2{
+  border: 1px solid #cccccc;
+  border-radius: 16rpx;
+  box-shadow: 5px 5px 5px #e5dfdf;
+  height: 350px;
+  width: 95%;
+  margin: 0 auto;
+}
+.box2{
+  height: 400px;
+}
+.image{
+  width: 100%;
+  height:164px;
+  display: flex;
+
+}
+.image view{
+  margin-left: 10rpx;
+}
+.image image{
+  height: 100%;
+  border-radius: 16rpx;
+  padding-left: 10px;
+}
+.list{
+  width: 100%;
+
+}
+.item{
+  width: 95%;
+  height: 33%;
+  display: flex;
+  margin: 0 auto;
+  border-bottom: 1px solid #cccccc;
+
+}
+.title,.input,.tips{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.item .title{
+  width: 20%;
+  height: 84rpx;
+}
+.item .input{
+  width: 65%;
+  height: 84rpx;
+  padding-left: 20rpx;
+
+}
+.item .tips{
+  width: 25%;
+  height: 84rpx;
+
+ color: pink;
+}
+.box1Btn{
+
+  height: 88rpx;
+  width: 95%;
+  margin: 20rpx auto;
+  display: flex;
+}
+.box1Btn .btn1 ,.btn2{
+   width: 45%;
+ }
+.box1Btn .btn1{
+  border: 1px solid #87cefa;
+  background: #ffffff;
+
+}
+.box1Btn .btn2{
+  opacity: 0.7;
+  background: #efebeb;
+
+}
+.box2Btn{
+  width: 95%;
+  margin: 20rpx auto;
+}
+.box2Btn button{
+  border: 1px solid #87cefa;
+
+}
+
+</style>
---
+++ src/pages/Sign/sign.vue
@@ -0,0 +1,128 @@
+<template>
+ <view class="box">
+
+   <view class="image align-center justify-center">
+   <img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwww.17qq.com%2Fimg_qqtouxiang%2F83456068.jpeg&refer=http%3A%2F%2Fwww.17qq.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621392654&t=7acb1fd423c41d7a927bedfe12177343" alt="">
+ </view>
+
+   <view class="input">
+     <input type="text" placeholder="QQ号/手机号/邮箱">
+     <input type="password" placeholder="密码" >
+
+   </view>
+
+   <button class="btn" :disabled="! isShow">登录</button>
+
+   <view class="forget">
+     <view class="left">忘记密码?</view>
+     <view class="right">新用户注册</view>
+   </view>
+
+   <view class="lastBox">
+     <view class="last">
+       <img src="../../static/true.png" height="15" width="15" v-if="isShow" @click="show"/>
+       <img src="../../static/checked.png" height="20" width="18" v-else @click="show"/>
+       <view>我已阅读协议</view>
+     </view>
+
+   </view>
+
+
+ </view>
+</template>
+
+<script>
+import TuiKeyboardInput from "@/components/thorui/tui-keyboard-input/tui-keyboard-input";
+export default {
+name: "sign",
+  components: {TuiKeyboardInput},
+  data(){
+    return{
+      isShow:true
+    }
+  },
+  methods:{
+    show(){
+      this.isShow=!this.isShow
+    }
+  }
+}
+</script>
+
+<style scoped>
+.box{
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 1350rpx;
+  /*background: yellow;*/
+  background:url("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpicnew13.photophoto.cn%2F20190507%2Fxiaoqingxinbeijingtu-33040572_1.jpg&refer=http%3A%2F%2Fpicnew13.photophoto.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621392403&t=2b129de3de7282b951fbeacbbb0dd2c1") no-repeat ;
+}
+.image{
+ margin-top: 200rpx;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 200rpx;
+}
+.image img{
+  width: 140rpx;
+  height: 140rpx;
+  border-radius: 50%;
+}
+.input{
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 40rpx;
+}
+.input input{
+  width: 70%;
+  height: 80rpx;
+  border: 1px black solid;
+  border-radius: 16rpx;
+  margin-top: 40rpx;
+}
+button{
+  margin-top: 40rpx;
+  width: 70%;
+  height: 80rpx;
+  border-radius: 20rpx;
+  color: #1d1c1c;
+}
+.forget{
+  width: 70%;
+  margin: 20rpx auto;
+  height: 40rpx;
+
+}
+.left{
+  float: left;
+  color: cornflowerblue;
+}
+.right{
+  float: right;
+  color: cornflowerblue;
+}
+.last img{
+  width: 30rpx;
+  height: 30rpx;
+  padding-top: 10rpx;
+
+}
+.lastBox{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.last view{
+  float: right;
+  margin-top: 5rpx;
+}
+
+
+
+</style>
