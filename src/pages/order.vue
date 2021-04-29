77fddedf19f8e426fe9b5824955ea916a4de420c
kongsa <www.zhengrenfu@hotmail.com>
2021-04-30 00:55:15 +0800

    :white_check_mark: 委托订单卡片任务

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,5 +1,7 @@
 <template>
     <view class="order">
+        <!-- 任务：订单卡片UI，时间：2021年4月30日00:53:33 -->
+        <!-- 这里的 {{datas}} 可以删除，只是做一个调试用的 -->
         {{ datas }}
     </view>
 </template>
@@ -12,6 +14,9 @@ export default {
             datas: []
         }
     },
+    /**
+     * 根据跳转页面的参数查询对应的订单类型。需登录才可以查询。
+     */
     onLoad(options) {
         options['userId'] = this.$store.state.userInfo.id
         this.$axios
@@ -19,7 +24,9 @@ export default {
             .then(resp => {
                 this.datas = resp.data
             })
-            .catch(error => {})
+            .catch(error => {
+                alert('没有登录！！！')
+            })
     }
 }
 </script>
