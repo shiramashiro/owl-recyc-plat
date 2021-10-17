75196a0a5b77129e822224537dcc216176e739a4
shiramashiro <zhengrenfu@outlook.com>
2021-10-17 18:11:06 +0800

    :art: 修改样式，删除多余组件，重组代码结构。

---
+++ src/assets/data/index.js
@@ -0,0 +1,73 @@
+const caskets = [
+    {
+        type: 'all',
+        icon: require('../../assets/icon/全部.png'),
+        title: '全部'
+    },
+    {
+        type: 'living',
+        icon: require('../../assets/icon/生活.png'),
+        title: '生活'
+    },
+    {
+        type: 'technology',
+        icon: require('../../assets/icon/科技.png'),
+        title: '科技'
+    },
+    {
+        type: 'social',
+        icon: require('../../assets/icon/社会.png'),
+        title: '社会'
+    },
+    {
+        type: 'business',
+        icon: require('../../assets/icon/经营.png'),
+        title: '经管'
+    },
+    {
+        type: 'literature',
+        icon: require('../../assets/icon/文学.png'),
+        title: '文学'
+    },
+    {
+        type: 'art',
+        icon: require('../../assets/icon/艺术.png'),
+        title: '艺术'
+    },
+    {
+        type: 'education',
+        icon: require('../../assets/icon/辅教.png'),
+        title: '辅教'
+    },
+    {
+        type: 'children',
+        icon: require('../../assets/icon/童书.png'),
+        title: '童书'
+    },
+    {
+        type: 'novel',
+        icon: require('../../assets/icon/小说.png'),
+        title: '小说'
+    },
+    {
+        type: 'foreign',
+        icon: require('../../assets/icon/外语.png'),
+        title: '外语'
+    },
+    {
+        type: 'audio',
+        icon: require('../../assets/icon/音像.png'),
+        title: '音像'
+    }
+]
+
+const broadcast = ['致力发展负责任的人工智能 中国发布八大治理原则', '600万吨包装纸产能砸向市场', '4月27日废纸价格最高上调50元/吨']
+
+const carousels = [
+    'https://p1.music.126.net/UZducXaYUzyukQuRxZ5rng==/109951166445121244.jpg?imageView&quality=89',
+    'https://p1.music.126.net/L6qUp1pk0CHe763ZWapSQQ==/109951166511220740.jpg?imageView&quality=89',
+    'https://p1.music.126.net/RdwUlDHKhfumKGerywHXew==/109951166511217995.jpg?imageView&quality=89',
+    'https://p1.music.126.net/1Z68-RH4zsT5cDxsXy56tw==/109951166511196968.jpg?imageView&quality=89'
+]
+
+export { caskets, broadcast, carousels }
