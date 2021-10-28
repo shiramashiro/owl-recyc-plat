59a972c1be16e7138c3a2e8fc0055e61d76bfa42
shiramashiro <zhengrenfu@outlook.com>
2021-10-28 21:50:56 +0800

    :construction: 创建帖子详情页

---
+++ src/pages/society/detail.vue
@@ -0,0 +1,21 @@
+<template>
+    <view class="detail">
+        <!-- 这是帖子详情页，点进去之后的页面，浏览器访问：localhost:8080/#/pages/society/detail -->
+
+        <!-- 可以仿造米游社的帖子详情页写 UI -->
+    </view>
+</template>
+
+<script>
+export default {
+    name: 'detail'
+}
+</script>
+
+<style lang="scss" scoped>
+// scss 写样式遵循和 template 中节点及子节点的层次结构。
+
+.detail {
+    // 这里写 detail 下的子节点
+}
+</style>
